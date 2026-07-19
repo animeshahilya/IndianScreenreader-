93f764b6a9b15aeb160983c38252ab2acefc0dd3
Alex <alex@example.com>
2026-07-19 19:07:16 +0530

    Fix build: update Chaquopy plugin to 16.0.0 resolved via Maven Central

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven("https://chaquo.com/maven")
     }
 }
 dependencyResolutionManagement {
@@ -11,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://chaquo.com/maven")
     }
 }
 
